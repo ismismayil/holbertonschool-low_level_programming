7f4fb1ad3390afaa3fb399ad5c446f2f7f897bdc
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-25 16:18:01 +0000

    task

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints a specific sentence using puts
  *
- * Return: Always 0 (Success)
+ * Return:  Always 0 (success)
  */
 int main(void)
 {
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", sizeof(d));
 printf("Size of an int: %lu byte(s)\n", sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-printf("Size of a float: %lu byte(s)\n", sizeof(e));	
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
 return (0);
 }
