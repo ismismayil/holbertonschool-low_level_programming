b08b9a297985363a42d0ecced908eed05c1e7819
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-26 16:05:13 +0000

    task

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int  main(void)
+{
+	char = is
+	for (is = 'a'; is <= 'z' ; is++)
+	{
+		putchar(is)
+	}
+	putchar(\n)
+	return (0)
+}
+
