67ad085b1d01a5da1abc25f4469dac713719bb81
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-27 14:16:20 +0000

    task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 void print_alphabet(void)
 {
