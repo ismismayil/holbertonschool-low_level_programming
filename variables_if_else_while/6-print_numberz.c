a564f2c29a4dbff7ecfb4a55f09b6a54558e4e96
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-27 11:16:29 +0000

    task

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int c;
 
 	for (c = 0; c <= 9; c++)
-
-		putchar(c);
-	
+		putchar(c + '0');	
 	putchar('\n');
 
 	return (0);
