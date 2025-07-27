851ac7fadbc62672028daa6d681ab3a5b74156c0
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-27 14:55:20 +0000

    task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * printt_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  *
  * Return: void
  */
