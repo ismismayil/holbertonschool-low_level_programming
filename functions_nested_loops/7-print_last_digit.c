e5352b5e77c7805110bbb94a9e4fd0fa0f216603
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-27 16:25:45 +0000

    task

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @c: The number to extract the last digit from.
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int c)
+{
+	c = abs(c);
+	c = c & 10;
+	return (c);
+}
