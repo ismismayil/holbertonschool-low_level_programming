76ca5b3c419fc5728bb5e69bdd60a28a909f2b77
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-28 12:34:16 +0000

    task

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n: The number of times the character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	for ( n; n >= 0; n-- )
+		
