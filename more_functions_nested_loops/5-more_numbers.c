0082a5bf43148b2e1d973a7f4f148a7444f9d2dc
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-28 11:05:46 +0000

    task

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int c;
+	int C;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (C = 0; C <= 14; C++)
+		{
+			_putchar(C + '0');
+		}
+		_putchar('\n');
+	}
+}
