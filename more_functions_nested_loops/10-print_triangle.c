db84e903a918aae2147705a25e6bb998f07146f8
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-30 14:03:09 +0000

    task

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle.
+ * @size: The size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, sym;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (sym = 1; sym <= size - row; sym++)
+			{
+				_putchar(' ');
+			}
+			for (sym = 1; sym <= row; sym++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
