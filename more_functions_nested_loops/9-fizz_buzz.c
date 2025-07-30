27deedc1392a50776ef1b96b6bc1b3ed855c7b47
ismismayil <ismayilzadeisim56@gmail.com>
2025-07-30 11:30:17 +0000

    task

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0)
+		if ((num % 15) == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if ((num % 5) == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((num % 15) == 0)
+		else if ((num % 3) == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -26,7 +26,10 @@ int main(void)
 		{
 			printf("%d", num);
 		}
-		printf(" ");
+		if (num < 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 
