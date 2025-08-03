939938af57f064f04b5872f0e86aefd65ff504a2
ismismayil <ismayilzadeisim56@gmail.com>
2025-08-03 14:15:50 +0000

    task

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * int _strlen - Returns the length of a string.
+ * @s: Pointer
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
