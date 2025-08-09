8b4a589ae8b54d76569f375d29b3f09b1ed1a196
ismismayil <ismayilzadeisim56@gmail.com>
2025-08-09 15:03:57 +0000

    task

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#inlcude "main.h"
+#include <stddef.h>
+/**
+ * *_strchr - Locates a character in a string.
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: *s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	for (;*s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
