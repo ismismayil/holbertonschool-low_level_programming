fa2dc9a210e453979216efb2dfff5fd66433114e
ismismayil <ismayilzadeisim56@gmail.com>
2025-08-17 15:31:39 +0000

    task

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *_strdup - Returns a heap-allocated copy of the given string.
+ * @str - null-terminated input string to duplicate
+ *
+ * Return: pointer to the duplicated string on success, or NULL on failure.
+ *         The returned buffer must be freed by the caller.
+ */
+char *_strdup(char *str)
+{
